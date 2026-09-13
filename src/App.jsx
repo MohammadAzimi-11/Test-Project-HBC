872c60a05c871f7d3ff7cc7741d872a3bc1b338d
MohammadAzimi-11 <mohammada5be3@gmail.com>
2026-09-13 10:10:52 +0430

    new style

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   return (
     <>
      <div>
-      <h1>Mohammad Azimi </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa voluptas animi, cupiditate sunt quaerat nihil numquam sint natus ab libero.</p>
-      <h2>Skill</h2>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Js</li>
-        <li>React.js</li>
+      <h1 className='text-3xl font-bold text-blue-500'>Mohammad Azimi </h1>
+      <p className='text-lg text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa voluptas animi, cupiditate sunt quaerat nihil numquam sint natus ab libero.</p>
+      <h2 className='text-2xl font-semibold text-gray-800'>Skill</h2>
+      <ul className='list-disc list-inside text-left'>
+        <li className='text-gray-600'>HTML</li>
+        <li className='text-gray-600'>CSS</li>
+        <li className='text-gray-600'>Js</li>
+        <li className='text-gray-600'>React.js</li>
       </ul>
      </div>
     </>
