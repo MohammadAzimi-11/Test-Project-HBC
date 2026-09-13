4b901791b10a9cf4aae4904c6319f5eda634b223
MohammadAzimi-11 <mohammada5be3@gmail.com>
2026-09-13 09:50:20 +0430

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>Mohammad Azimi </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa voluptas animi, cupiditate sunt quaerat nihil numquam sint natus ab libero.</p>
+      <h2>Skill</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Js</li>
+        <li>React.js</li>
+      </ul>
+     </div>
+    </>
+  )
+}
+
+export default App
